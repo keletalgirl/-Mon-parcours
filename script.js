a618ba01e693b8452fbdff5c9d0941232ce28b4a
lmsb <hafssa.lakhal27@gmail.com>
2026-07-21 12:19:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -37,3 +37,85 @@ function playVideo() {
 
     video.play();
 }
+let ensibsPhotos = [
+    "ensibs.jpg",
+    "ensibs1.jpg",
+    "ensibs2.jpg",
+    "ensibs3.jpg"
+];
+
+
+let paulPhotos = [
+    "paulva.jpg",
+    "paulva1.jpg",
+    "paulva2.jpg",
+    "paulva3.jpg"
+];
+
+
+let indexEnsibs = 0;
+let indexPaul = 0;
+
+
+
+setInterval(()=>{
+
+
+let img = document.getElementById("ensibs-slider");
+
+
+if(img){
+
+indexEnsibs++;
+
+if(indexEnsibs >= ensibsPhotos.length){
+indexEnsibs = 0;
+}
+
+
+img.classList.remove("fade-image");
+
+void img.offsetWidth;
+
+img.src = ensibsPhotos[indexEnsibs];
+
+img.classList.add("fade-image");
+
+
+}
+
+
+},3500);
+
+
+
+
+
+setInterval(()=>{
+
+
+let img = document.getElementById("paul-slider");
+
+
+if(img){
+
+indexPaul++;
+
+if(indexPaul >= paulPhotos.length){
+indexPaul = 0;
+}
+
+
+img.classList.remove("fade-image");
+
+void img.offsetWidth;
+
+img.src = paulPhotos[indexPaul];
+
+img.classList.add("fade-image");
+
+
+}
+
+
+},3500);
